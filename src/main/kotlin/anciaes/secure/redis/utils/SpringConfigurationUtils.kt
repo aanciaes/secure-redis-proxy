7ebe4b76483e7b2755a0bf25fb9502c47ea889c5
aanciaes <aanciaes.miguel@gmail.com>
2020-06-30 17:58:36 +0100

    Lint

--- src/main/kotlin/anciaes/secure/redis/utils/SpringConfigurationUtils.kt
+++ src/main/kotlin/anciaes/secure/redis/utils/SpringConfigurationUtils.kt
@@ -9,8 +9,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
-import org.springframework.web.filter.CommonsRequestLoggingFilter
-
 
 @Component
 object SpringConfigurationUtils {
