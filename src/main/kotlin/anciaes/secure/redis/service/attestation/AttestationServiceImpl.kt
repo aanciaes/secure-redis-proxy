bb0a738920ea6103ad43efe006ec29c19b7de63c
aanciaes <aanciaes.miguel@gmail.com>
2020-11-11 15:35:45 +0000

    Bumped version to project to 1.0.0

--- src/main/kotlin/anciaes/secure/redis/service/attestation/AttestationServiceImpl.kt
+++ src/main/kotlin/anciaes/secure/redis/service/attestation/AttestationServiceImpl.kt
@@ -43,7 +43,7 @@ class AttestationServiceImpl : AttestationService {
     val proxyIdentityKeyNamePassword: String
         get() = if (activeProfile == "prod") "Gwb!KMcF37rYHsTmHiLkFs9ms" else "Lcq6jCG-GFhnfqLK4PhyvWFj_"
     val proxyJarChallenge: String
-        get() = if (activeProfile == "prod") "/home/secure-proxy-redis/secure-redis-proxy-0.4.1.jar" else "mock-files/mock-proxy-java-jar"
+        get() = if (activeProfile == "prod") "/home/secure-proxy-redis/secure-redis-proxy-1.0.0.jar" else "mock-files/mock-proxy-java-jar"
     val proxyMrEnclaveChallenge: String
         get() = if (activeProfile == "prod") "/home/secure-proxy-redis/mrenclave" else "mock-files/mrenclave-mock"
     val cpuInfoFilePath: String
